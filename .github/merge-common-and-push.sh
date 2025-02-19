b2efb4fde7048aa10aa66fc73da869e422788f21
nanashi <na@na.shi>
2025-02-19 17:07:17 +0800

    fix: add debug output

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -2,26 +2,29 @@
 
 set -e -o pipefail
 
-function mergeCommonAndPush() {
-	git config --local user.email "github-action@users.noreply.github.com"
-	git config --local user.name "GitHub Action"
-	git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
+echo before config user name and email
 
-	fmt='
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
+git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
+
+echo before set fmt
+
+fmt='
 	git checkout %(refname:lstrip=3)
 	git restore --source=origin/common --worktree .
 	git add --all -- :\!basic-config/hardware.nix :\!.gitignore :\!.github
 	git commit -m "update: merge common config"
 	git push github %(refname:lstrip=3)
-	'
+'
 
-	eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
+echo before set eval
 
-	# for debug
-	echo "$eval"
+eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
 
-	eval "$eval"
-}
+echo before eval
 
-mergeCommonAndPush 
+eval "$eval"
+
+echo after eval
 
