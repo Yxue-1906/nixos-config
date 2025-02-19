52a80e9f5a43c03d081bd1faeca1381dea44a0c2
nanashi <na@na.shi>
2025-02-19 18:20:54 +0800

    clean: clean debug output

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -7,20 +7,12 @@ git config --local user.name "GitHub Action"
 git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
 
 fmt='
-        echo %(refname:lstrip=3)
 	git checkout %(refname:lstrip=3)
-	echo after checkout 
 	git restore --source=origin/common --worktree .
-	echo after restore
 	git add --all -- :\!basic-config/hardware.nix :\!.gitignore :\!.github
-	echo after add
 	git commit -m "update: merge common config"
-	echo after commit
 	git push github %(refname:lstrip=3)
-	echo after push
 '
-
-git for-each-ref --shell --format='%(refname)' 'refs/remotes/origin/*'
 
 eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
 
