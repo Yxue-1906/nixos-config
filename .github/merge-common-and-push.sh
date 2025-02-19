7d6e0b3718bd387acfa98666bbf06c6f7038f8f7
nanashi <na@na.shi>
2025-02-19 18:06:36 +0800

    fix: try find bug

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -2,13 +2,9 @@
 
 set -e -o pipefail
 
-echo before config user name and email
-
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
 git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
-
-echo before set fmt
 
 fmt='
         echo %(refname:lstrip=3)
@@ -24,13 +20,8 @@ fmt='
 	echo after push
 '
 
-echo before set eval
+git for-each-ref --shell --format='%(refname)' 'refs/remotes/origin/*'
 
 eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
 
-echo before eval
-
 eval "$eval"
-
-echo after eval
-
