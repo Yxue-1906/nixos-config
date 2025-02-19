8d7dff10d662d6c4125ab7012ca67ec3de10baf6
nanashi <na@na.shi>
2025-02-19 16:46:21 +0800

    fix: try fix

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-git config --local user.email "github-action@users.noreply.github.com"
-git config --local user.name "GitHub Action"
-git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
+set -e -o pipefail
 
-fmt='
-     git checkout %(refname:lstrip=3)
-     git restore --source=origin/common --worktree .
-     git add --all -- :\!basic-config/hardware.nix :\!.gitignore :\!.github
-     git commit -m "update: merge common config"
-     git push github %(refname:lstrip=3)
-'
+function mergeCommonAndPush() {
+	git config --local user.email "github-action@users.noreply.github.com"
+	git config --local user.name "GitHub Action"
+	git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
 
-eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
+	fmt='
+	git checkout %(refname:lstrip=3)
+	git restore --source=origin/common --worktree .
+	git add --all -- :\!basic-config/hardware.nix :\!.gitignore :\!.github
+	git commit -m "update: merge common config"
+	git push github %(refname:lstrip=3)
+	'
 
-# for debug
-echo "$eval"
+	eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/remotes/origin/common --exclude=refs/remotes/origin/HEAD refs/remotes/origin/*`
 
-eval "$eval"
+	# for debug
+	echo "$eval"
+
+	eval "$eval"
+}
+
+mergeCommonAndPush 
+
