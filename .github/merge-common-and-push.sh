429151535f7068c7f2d6a625cb8150643a336200
nanashi <na@na.shi>
2025-02-19 17:42:14 +0800

    fix: try fix

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -11,11 +11,17 @@ git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/n
 echo before set fmt
 
 fmt='
+        echo %(refname:lstrip=3)
 	git checkout %(refname:lstrip=3)
+	echo after checkout 
 	git restore --source=origin/common --worktree .
+	echo after restore
 	git add --all -- :\!basic-config/hardware.nix :\!.gitignore :\!.github
+	echo after add
 	git commit -m "update: merge common config"
+	echo after commit
 	git push github %(refname:lstrip=3)
+	echo after push
 '
 
 echo before set eval
