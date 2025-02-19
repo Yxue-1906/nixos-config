523fe36f2a1e7eef25507bfce92c323cb7758a7c
nanashi <na@na.shi>
2025-02-19 15:09:40 +0800

    fix: try get output

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
 git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-config.git
 
 fmt='
