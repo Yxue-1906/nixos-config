fd94c1f811720a615e666d1afbf67be564ae8ab6
nanashi <na@na.shi>
2025-02-19 13:01:50 +0800

    fix: fix bug in merge script

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -2,15 +2,16 @@
 
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
+git remote add github https://github-action:$GITHUB_TOKEN@github.com/Yxue-1906/nixos-configs.git
 
 fmt='
      git checkout %(refname:short)
      git restore --source=common --worktree .
      git add --all -- ":!basic-config/hardware.nix" ":!.gitignore" ":!.github"
      git commit -m "update: merge common config"
-     git push
+     git push github %(refname:short)
 '
 
-eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/heads/master refs/heads/*`
+eval=`git for-each-ref --shell --format="$fmt" --exclude=refs/heads/common refs/heads/*`
 
 eval "$eval"
