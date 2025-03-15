a5df6c4b5b870df0e045fefca390d884e9ffcec0
Yxue-1906 <20736038+Yxue-1906@users.noreply.github.com>
2025-03-16 01:47:28 +0800

    Feat update merge script (#20)
     - use $(...) to replace \`...\`
     - add branch exclude prefix `bump-` for version bump branch
     - also add prefix `revert-` for revert commits
    
    ---------
    
    Co-authored-by: nanashi <na@na.shi>

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -18,11 +18,12 @@ fmt='
 # replace `...` with $(...)
 # see https://mywiki.wooledge.org/BashFAQ/082
 eval=$(git for-each-ref --shell --format="$fmt"\
-
+\
 	--exclude=refs/remotes/origin/fix-*\
 	--exclude=refs/remotes/origin/feat-*\
 	--exclude=refs/remotes/origin/bump-*\
-
+	--exclude=refs/remotes/origin/revert-*\
+\
 	--exclude=refs/remotes/origin/common\
 	--exclude=refs/remotes/origin/HEAD\
 	refs/remotes/origin/*)
