cd5cb6a4f3d98898ee36a3029b3f59f370dff6d9
Yxue-1906 <20736038+Yxue-1906@users.noreply.github.com>
2025-04-30 16:23:59 +0800

    chore: remove unused input (#41)
    Co-authored-by: nanashi <na@na.shi>

--- flake.nix
+++ flake.nix
@@ -9,11 +9,10 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-gvfs-1-54-2.url = "github:NixOS/nixpkgs/99a83d3cc8a962701b76ee1fb7b6799dd04e34a8";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/9df5ff73a8887edc8fb69a9facf3b7f6e8ed17d7";
     nix-alien.url = "github:thiagokokada/nix-alien/master";
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixpkgs-gvfs-1-54-2, ... }@inputs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
     with nixpkgs.lib; 
     {
       nixosConfigurations."unrelated" = nixosSystem rec {
