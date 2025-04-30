b9e79d7c49c103d1d4868ce99202e9d2edce1869
nanashi <na@na.shi>
2025-04-30 16:18:12 +0800

    feat: add exclude merge branch schema

--- .github/merge-common-and-push.sh
+++ .github/merge-common-and-push.sh
@@ -23,6 +23,7 @@ eval=$(git for-each-ref --shell --format="$fmt"\
 	--exclude=refs/remotes/origin/feat-*\
 	--exclude=refs/remotes/origin/bump-*\
 	--exclude=refs/remotes/origin/revert-*\
+	--exclude=refs/remotes/origin/chore-*\
 \
 	--exclude=refs/remotes/origin/common\
 	--exclude=refs/remotes/origin/HEAD\
