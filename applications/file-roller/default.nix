aa8c19f567d67cd6ad4a73c691fdffa2f9836f0a
nanashi <na@na.shi>
2025-05-02 13:26:22 +0800

    fix: disable libarchive

--- applications/file-roller/default.nix
+++ applications/file-roller/default.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   nixpkgs.overlays = [
     (final: prev: {
       file-roller = prev.file-roller.overrideAttrs (finalAttrs: prevAttrs: {
+        mesonFlags = [
+	  (lib.mesonEnable "libarchive" false)
+	];
         patches = (prevAttrs.patches or []) ++ [
 	  ./restore-extract-button-behavior.patch
 	];
