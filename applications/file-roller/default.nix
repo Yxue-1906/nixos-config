0ffde8db03cee8330b60c57bfa529374d32632b1
nanashi <na@na.shi>
2025-05-03 02:40:53 +0800

    fix: add patch to fix file-roller read rar output incorrectly

--- applications/file-roller/default.nix
+++ applications/file-roller/default.nix
@@ -1,12 +1,19 @@
 { pkgs, lib, ... }: {
+  environment.systemPackages = with pkgs; [
+    # provide rar support
+    rar
+  ];
   nixpkgs.overlays = [
     (final: prev: {
       file-roller = prev.file-roller.overrideAttrs (finalAttrs: prevAttrs: {
         mesonFlags = [
+	  # disable libarchive
 	  (lib.mesonEnable "libarchive" false)
 	];
         patches = (prevAttrs.patches or []) ++ [
 	  ./restore-extract-button-behavior.patch
+	  # original implement cant read filename correctly if size to long
+	  ./fix-read-name-incorrectly-when-size-to-long.patch
 	];
       });
     })
