5c0b021823f0a73641aaefa50443112c0f76af8d
nanashi <na@na.shi>
2025-05-30 15:41:06 +0800

    feat(networking): move some config

--- networking/default.nix
+++ networking/default.nix
@@ -1,4 +1,10 @@
 {...}: {
+
+  networking.hostName = "unrelated";
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
   imports = [
     ./networking.nix
   ];
--- networking/networking.nix
+++ networking/networking.nix
@@ -1,10 +1,4 @@
 { self, pkgs, config, lib, secrets, ...}: {
-
-  networking.hostName = "unrelated";
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
   networking.firewall = {
     trustedInterfaces = [ "singbox_tun" ];
     checkReversePath = "loose";
