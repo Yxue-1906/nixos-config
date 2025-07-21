6f36aa5dbb731d187e9b10f325932f6da721c0af
nanashi <na@na.shi>
2025-07-21 23:27:01 +0800

    feat: use x11 instead of wayland to gain more stable experience

--- basic-config/window.nix
+++ basic-config/window.nix
@@ -4,14 +4,15 @@
     enable = true;
     displayManager.gdm = {
       enable = true;
+      wayland = false;
     };
     desktopManager.gnome = {
       enable = true;
-      extraGSettingsOverridePackages = [ pkgs.mutter ];
-      extraGSettingsOverrides = ''
-        [org.gnome.mutter]
-        experimental-features=['scale-monitor-framebuffer']
-      '';
+      # extraGSettingsOverridePackages = [ pkgs.mutter ];
+      # extraGSettingsOverrides = ''
+      #   [org.gnome.mutter]
+      #   experimental-features=['scale-monitor-framebuffer']
+      # '';
     };
     xkb = {
       layout = "cn";
